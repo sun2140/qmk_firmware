d8925db43986857687876dbc310f9adc87526aea
Nicolas GORAJ <github@goraj.fr>
2025-03-09 22:10:04 +0100

    feat: a lot of things

--- keyboards/beekeeb/3w6hs/keymaps/sun2140/keymap.c
+++ keyboards/beekeeb/3w6hs/keymaps/sun2140/keymap.c
@@ -24,6 +24,7 @@
 
 // Left hand
 #define LT_Q LT(_CFG,FR_Q)      // Top row
+#define S_TAB S(KC_TAB)
 
 #define LGUI_XX LGUI_T(XXXXXXX) // Home row
 #define LALT_XX LALT_T(XXXXXXX) // Home row
@@ -35,9 +36,9 @@
 #define LT_E LCTL_T(FR_E) // Home row
 #define LT_N LSFT_T(FR_N) // Home row
 
-#define LT_REP LT(_FUNC, QK_REP)   // Thumb cluster
-#define LT_BSPC LT(_NUM, KC_BSPC)  // Thumb cluster
-#define LT_ENT KC_ENT     // Thumb cluster
+#define LT_REP LT(_NAV_NUM, KC_F20)   // Thumb cluster
+#define LT_BSPC LSFT_T(KC_BSPC)  // Thumb cluster
+#define LT_ENT LT(_SYM, KC_ENT)     // Thumb cluster
 
 // Right hand
 #define OSL_DK OSL(_DEAD)  // Top row
@@ -52,15 +53,15 @@
 #define LT_I RALT_T(FR_I) // Home row
 #define LT_U RGUI_T(FR_U) // Home row
 
-#define LT_TAB KC_TAB  // Thumb cluster
-#define LT_SPC KC_SPC     // Thumb cluster
+
+#define LT_SPC LT(_NAV_NUM, KC_SPC)     // Thumb cluster
 #define LT_ESC LT(_SYM,KC_ESC)   // Thumb cluster
 
 enum layers {
     _ALPHA,
     _SYM,
-    _NUM,
-    _FUNC,
+    _NAV_NUM,
+    _FUN,
     _DEAD,
     _CFG
 };
@@ -71,41 +72,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            LT_Q,    FR_C,    FR_O,    FR_P,    FR_W,            FR_J,    FR_M,    FR_D,  OSL_DK,    FR_Y,
            LT_A,    LT_S,    LT_E,    LT_N,    FR_F,            FR_L,    LT_R,    LT_T,    LT_I,    LT_U,
            FR_Z,    FR_X, FR_MINS,    FR_V,    FR_B,          FR_DOT,    FR_H,    FR_G, FR_COMM,    FR_K,
-                           LT_REP, LT_BSPC,  LT_ENT,          LT_TAB,  LT_SPC,  LT_ESC
+                          XXXXXXX, LT_BSPC,  LT_REP,          LT_SPC,  LT_ENT, XXXXXXX
     ),
     [_SYM] = LAYOUT_split_3x5_3(
-        FR_QUOT,  FR_GRV, FR_DQUO, FR_CIRC, FR_TILD,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        FR_LABK, FR_LCBR, FR_LPRN, FR_LBRC, FR_BSLS,         XXXXXXX, RSFT_XX, RCTL_XX, RALT_XX, RGUI_XX,
-        XXXXXXX, FR_HASH,   FR_AT, FR_AMPR, FR_PIPE,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                          _______, _______,  FR_DLR,         XXXXXXX, XXXXXXX, _______
+        FR_CIRC, FR_LABK, FR_RABK,  FR_DLR, FR_PERC,           FR_AT, FR_AMPR, FR_ASTR, FR_QUOT,  FR_GRV,
+        FR_LCBR, FR_LPRN, FR_RPRN, FR_RCBR,  FR_EQL,         FR_BSLS, FR_PLUS, FR_MINS, FR_SLSH, FR_DQUO,
+        FR_TILD, FR_LBRC, FR_RBRC, FR_UNDS, FR_HASH,         FR_PIPE, FR_EXLM, FR_SCLN, FR_COLN, FR_QUES,
+                          _______, _______, _______,         _______, _______, _______
     ),
-    [_NUM] = LAYOUT_split_3x5_3(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,    FR_7,    FR_8,    FR_9, XXXXXXX,
-        LGUI_XX, LALT_XX, LCTL_XX, LSFT_XX, XXXXXXX,          FR_EQL,    FR_4,    FR_5,    FR_6, FR_SLSH,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,    FR_1,    FR_2,    FR_3, XXXXXXX,
-                          XXXXXXX, _______, XXXXXXX,         FR_PERC,    FR_0, FR_UNDS
+    [_NAV_NUM] = LAYOUT_split_3x5_3(
+         KC_TAB, KC_HOME,   KC_UP,  KC_END, KC_PGUP,         FR_SLSH,    FR_7,    FR_8,    FR_9, XXXXXXX,
+        XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,         FR_MINS,    FR_4,    FR_5,    FR_6,    FR_0,
+        XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU,   S_TAB,         FR_COMM,    FR_1,    FR_2,    FR_3,  FR_DOT,
+                          _______,  KC_DEL, _______,         _______,  KC_ESC, _______
     ),
-    [_FUNC] = LAYOUT_split_3x5_3(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         KC_PSCR,   KC_F7,   KC_F8,   KC_F9,  KC_F12,
-        LGUI_XX, LALT_XX, LCTL_XX, LSFT_XX, XXXXXXX,         XXXXXXX,   KC_F4,   KC_F5,   KC_F6,  KC_F11,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         KC_PAUS,   KC_F1,   KC_F2,   KC_F3,  KC_F10,
-                          _______, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX
+    [_FUN] = LAYOUT_split_3x5_3(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F12,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,   KC_F4,   KC_F5,   KC_F6,  KC_F11,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,   KC_F1,   KC_F2,   KC_F3,  KC_F10,
+                          _______, _______, _______,         _______, _______, _______
     ),
     [_DEAD] = LAYOUT_split_3x5_3(
-        XXXXXXX, FR_CCED,   FR_OE, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX,  QK_RBT, XXXXXXX,
-          FR_AE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, RSFT_XX, RCTL_XX, RALT_XX, RGUI_XX,
+        XXXXXXX, FR_CCED,   FR_OE, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+          FR_AE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                          XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX
+                          _______, _______, _______,         _______, _______, _______
     ),
     [_CFG] = LAYOUT_split_3x5_3(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX,  QK_RBT, QK_BOOT,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                          XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX
+                          _______, _______, _______,         _______, _______, _______
     ),
 };
 
-const key_override_t shift_backspace_is_del =            ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 const key_override_t shift_hyphen_is_question_mark =     ko_make_basic(MOD_MASK_SHIFT, FR_MINS, FR_QUES);
 const key_override_t shift_dot_is_colon =                ko_make_basic(MOD_MASK_SHIFT, FR_DOT, FR_COLN);
 const key_override_t shift_comma_is_semicolon =          ko_make_basic(MOD_MASK_SHIFT, FR_COMM, FR_SCLN);
@@ -129,7 +129,6 @@ const key_override_t shift_ae_is_capital_ae = ko_make_basic(MOD_MASK_SHIFT, FR_A
 const key_override_t *key_overrides[] = {
     &shift_osl_dead_key_is_exclamation,
     &shift_rep_is_alt_rep,
-    &shift_backspace_is_del,
     &shift_hyphen_is_question_mark,
     &shift_dot_is_colon,
     &shift_comma_is_semicolon,
@@ -142,3 +141,23 @@ const key_override_t *key_overrides[] = {
     &shift_oe_is_capital_oe,
     &shift_ae_is_capital_ae
 };
+
+bool remember_last_key_user(uint16_t keycode, keyrecord_t* record, uint8_t* remembered_mods) {
+    if (keycode == LT_REP) {
+        return false;
+    }
+    return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    switch (keycode) {
+        case LT_REP:
+            if (record->tap.count) {  // On tap.
+                // alt_repeat_key_invoke(&record->event);
+                repeat_key_invoke(&record->event);
+                return false;  // Skip default handling.
+            }
+            return true;  // Continue default handling.
+        }
+    return true;
+}
